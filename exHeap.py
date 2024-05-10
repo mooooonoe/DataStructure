ab02d56cf1255a16f9e0441df7566c77b80b0948
mooooonoe <msb3536@naver.com>
2024-05-10 09:30:45 +0900

    힙 예제 1번

---
+++ exHeap.py
@@ -0,0 +1,13 @@
+# 1. 철수 영희 문제 
+# 파이썬 내부에서 제공해주는 힙 사용 하려고 합니다 ~  -> heapq : min heap 을 제공하는데 우선 순위가 낮은 값부터 제거하는 형식 ~
+ 
+import heapq
+
+A = [1,4,5,2,3]
+
+hq.heapify(A)
+
+print(A)
+
+a = hq.heapop(A)
+print(a)
